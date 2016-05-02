cae8d17a18fad7ce55cdc81df30a329c7f1baf30
Habib <hbrawnak@gmail.com>
2016-05-02 22:31:19 +0600

    no need

--- controller/HomeController.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    get_index : function(req, res) {
-        connection.query('SELECT * FROM insert_data', function (err, rows) {
-            res.render('index', {items: rows});
-        });
-    }
-};
